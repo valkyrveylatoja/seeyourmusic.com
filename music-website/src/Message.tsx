94824d404b6772e93a1b258961e95caf549c5de7
Val Kyrvey Latoja <valkyrveylatoja@gmail.com>
2025-10-31 11:22:21 +0400

    Learning React Basics

---
+++ music-website/src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+function Message() {
+    // JSX: JavaScript XML
+    const name = 'val'
+    if (name)
+        return <h1>hi {name} :3</h1>;
+    return <h1> hi person who has no name :3</h1>
+}
+
+export default Message;
